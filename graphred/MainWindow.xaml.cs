d102ec5090a1d17ddcfb8633c26e1700c347cc05
kiryshaboshki <danilovkirill58@gmail.com>
2025-10-31 23:00:25 +1000

    рисование

--- graphred/MainWindow.xaml.cs
+++ graphred/MainWindow.xaml.cs
@@ -9,16 +9,52 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace graphred
+namespace graphedit
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private bool IsDrawing = false;
+        private Point startPoint;
+        private Brush CurrentBrush = Brushes.Black;
+        private double BrushSize = 2;
+
         public MainWindow()
         {
+
             InitializeComponent();
         }
+
+        private void DrawCanvasMouseDown(object sender, MouseEventArgs e)
+        {
+            IsDrawing = true;
+            startPoint = e.GetPosition(DrawCanvas);
+        }
+
+        private void DrawCanvasMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!IsDrawing) return;
+
+            Point CurrentPoint = e.GetPosition(DrawCanvas);
+
+            Line line = new Line();
+            line.X1 = startPoint.X;
+            line.Y1 = startPoint.Y;
+
+            line.X2 = CurrentPoint.X;
+            line.Y2 = CurrentPoint.Y;
+
+            line.Stroke = CurrentBrush;
+            line.StrokeThickness = BrushSize;
+
+            DrawCanvas.Children.Add(line);
+
+            startPoint = CurrentPoint;
+        }
+
+        private void DrawCanvasMouseUp(object sender, MouseEventArgs e)
+        {
+            IsDrawing = false;
+        }
+            
     }
 }
